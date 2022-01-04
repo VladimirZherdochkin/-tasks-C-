629759fd301a9494f72b12d11c757d17b6a71da0
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-04 18:30:36 +0300

    Даны два числа. Показать большее и меньшее число

---
+++ primer2(/Program.cs
@@ -0,0 +1,15 @@
+﻿//Даны два числа. Показать большее и меньшее число
+int a, b;
+Console.WriteLine("Введите два числа : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("{0} наибольшее число", a);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ", b);
+}
+Console.ReadLine();
+
