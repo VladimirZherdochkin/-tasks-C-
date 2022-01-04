f2ed6fd832b702a3773a086d42068b67a377f735
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-04 23:23:32 +0300

    Найти максимальное из трех чисел

---
+++ primer4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Найти максимальное из трех чисел
+int a, b, c;
+Console.WriteLine("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > a) max = b;
+if (c > max) max = c;
+Console.WriteLine(max);
