4b69242689fb02ae6b4e5527d460f1f74cfec066
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-04 19:00:10 +0300

    По заданному номеру дня недели вывести его название

---
+++ primer3/Program.cs
@@ -0,0 +1,14 @@
+﻿//По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите номер дня недели - ");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1: Console.WriteLine("понедельник"); break;
+    case 2: Console.WriteLine("вторник"); break;
+    case 3: Console.WriteLine("среда"); break;
+    case 4: Console.WriteLine("четверг"); break;
+    case 5: Console.WriteLine("пятница"); break;
+    case 6: Console.WriteLine("суббота"); break;
+    case 7: Console.WriteLine("воскресенье"); break;
+    default: Console.WriteLine("ВЫ ОШИБЛИСЬ"); break;
+}
