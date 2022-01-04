30688c980ae887e4fc8ae0984b2059861e1946d4
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-04 16:03:52 +0300

    kvadrat chisla

---
+++ primer1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вывести квадрат числа
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(Math.Pow(numberA, 2));
+// int result=numberA*numberA;
+// Console.WriteLine(result);
