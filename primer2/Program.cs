7399b09359be1c609b9e48afcc752e63e434f9e9
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-04 17:05:57 +0300

    По двум заданным числам проверять является ли первое квадратом второго

---
+++ primer2/Program.cs
@@ -0,0 +1,14 @@
+﻿//По двум заданным числам проверять является
+// ли первое квадратом второго
+Console.WriteLine("Введите первое число");
+int numbersA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numbersB = Convert.ToInt32(Console.ReadLine());
+if (numbersA == Math.Pow(numbersB, 2) || numbersB == Math.Pow(numbersA, 2))
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
