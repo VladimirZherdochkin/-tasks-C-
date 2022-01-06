5fc25205f60060d74302d3a057858d52856e606a
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-06 15:14:54 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ primer13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a % b;
+if (result != 0)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Кратно");
+}
