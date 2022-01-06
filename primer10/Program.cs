7736dc7fc185ccb282e867e679e95888c6d9c0d2
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-06 14:02:15 +0300

    Показать вторую цифру трёхзначного числа

---
+++ primer10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (99 < a && a < 1000)
+{
+    int result = (a % 100)/10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Ввели число не соответсвующее условию");
+}
