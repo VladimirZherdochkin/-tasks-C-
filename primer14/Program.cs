48906dc996f4bc199e097aa5b044b4f25067a552
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-06 15:28:22 +0300

    Найти третью цифру числа или сообщить, что её нет

---
+++ primer14/Program.cs
@@ -0,0 +1,12 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if(99<a)
+{
+    int result = a % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Последней цифры нет");
+}
