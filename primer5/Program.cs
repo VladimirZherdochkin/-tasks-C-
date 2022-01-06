4ee6ee8802f4968864f9919c2a6b7c19fbf58d01
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-06 12:50:34 +0300

    Написать программу вычисления значения функции y = f(a)=(1+1/a)^a

---
+++ primer5/Program.cs
@@ -0,0 +1,13 @@
+﻿//Написать программу вычисления значения функции y = f(a)=(1+1/a)^a
+Console.WriteLine("Введите переменную а=");
+double a = Convert.ToDouble(Console.ReadLine());
+double y = 0;
+if (a == 0)
+{
+    Console.WriteLine("Число не корректно!");
+}
+else
+{
+    y = Math.Pow((1 + 1 / a),a);
+}
+Console.WriteLine($"{y}");
