89de2613b93fa2a4e7065c0784538ccc3b95ad67
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-06 14:19:49 +0300

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ primer11/Program.cs
@@ -0,0 +1,13 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random().Next(10,100);
+Console.WriteLine(number);
+int a = number %10;
+int b = number /10;
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
