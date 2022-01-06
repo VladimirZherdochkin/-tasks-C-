d08a1e35bfa0cc0c9589eba9a9807d60e35bf1d2
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-06 13:48:30 +0300

    Показать последнюю цифру трёхзначного числа

---
+++ primer9/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (99 < a && a < 1000)
+{
+    int result = a % 10;
+    Console.WriteLine(result);
+}
