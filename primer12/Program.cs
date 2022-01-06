f27dea1019877595b7cbbd423b959d93c5e7714a
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-06 14:59:43 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ primer12/Program.cs
@@ -0,0 +1,7 @@
+﻿//Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result1 = number % 10;
+int result2 = (number % 1000) / 100;
+Console.WriteLine("После удаления второй цыфры получился следующий результат: " + result2 + result1);
+
