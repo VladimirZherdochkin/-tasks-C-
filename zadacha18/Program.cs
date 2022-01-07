a698052cbd4f5798b50a21e63a3d5a330adf102f
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-07 18:44:54 +0300

    Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y

---
+++ zadacha18/Program.cs
@@ -0,0 +1,7 @@
+﻿//Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+Console.WriteLine("Введите x");
+bool x = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("Введите y");
+bool y = Convert.ToBoolean(Console.ReadLine());
+bool result = (!(x || y)) == (!x && !y);
+Console.WriteLine(result);
