36e875a7461c1bccee297fdd108f86f443d4701a
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-08 19:42:31 +0300

    Найти кубы чисел от 1 до N

---
+++ zadacha24/Program.cs
@@ -0,0 +1,11 @@
+﻿//Найти кубы чисел от 1 до N
+System.Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i * i * i <= N; i++)
+{
+    Console.Write($"[{i}]={i * i * i},");
+    if (i % 5 == 0)
+    {
+        System.Console.WriteLine();
+    }
+}
