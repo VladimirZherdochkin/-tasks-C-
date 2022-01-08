1a96b5d56501961f9839b1db90f81d1f85503488
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-08 19:37:03 +0300

    Показать таблицу квадратов чисел от 1 до N

---
+++ zadacha23/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать таблицу квадратов чисел от 1 до N
+System.Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i * i <= N; i++)
+{
+    Console.Write($"[{i}]={i * i},");
+    if (i % 5 == 0)
+    {
+        System.Console.WriteLine();
+    }
+}
