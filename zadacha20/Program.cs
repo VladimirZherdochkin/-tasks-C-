98dc693cbc95c8a46dc98c5ae2e8354622740693
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-08 18:19:47 +0300

    Задать номер четверти, показать диапазоны для возможных координат

---
+++ zadacha20/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер четверти");
+int chetvert = Convert.ToInt32(Console.ReadLine());
+if (chetvert == 1)
+{
+    Console.WriteLine("(x > 0) && (y > 0)");
+}
+if (chetvert == 2)
+{
+    Console.WriteLine("(x < 0) && (y > 0)");
+}
+if (chetvert == 3)
+{
+    Console.WriteLine("(x < 0) && (y < 0)");
+}
+if (chetvert == 4)
+{
+    Console.WriteLine("(x > 0) && (y < 0)");
+}
