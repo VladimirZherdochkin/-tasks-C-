8a37b793fd6a1248f5f33942874752ae3c6e6e41
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-08 18:59:43 +0300

    Программа проверяет пятизначное число на палиндромом

---
+++ zadacha21/Program.cs
@@ -0,0 +1,27 @@
+﻿//Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int reverse = 0;
+int remainder = 0;
+int count = 0;
+    while (number > reverse)
+    {
+        remainder = number % 10;
+        reverse = reverse * 10 + remainder;
+        number = number / 10;
+        count++;
+    }
+    Console.WriteLine(count);
+    if (reverse == number)
+    {
+        Console.WriteLine("Your number is a palindrome");
+    }
+    else
+    {
+        number = number * 10 + remainder;
+        if (reverse == number)
+            Console.WriteLine("your number is a palindrome");
+        else
+            Console.WriteLine("your number is not a palindrome");
+    }
+    Console.ReadLine();
