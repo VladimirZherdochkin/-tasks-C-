b2c97f63dba781d86781cce12172a5ff0eabd641
VladimirZherdochkin <95964228+VladimirZherdochkin@users.noreply.github.com>
2022-01-08 20:02:07 +0300

    Найти сумму чисел от 1 до А

---
+++ zadacha25/Program.cs
@@ -0,0 +1,12 @@
+﻿
+System.Console.WriteLine("Введите число N");
+long N = Convert.ToInt64(Console.ReadLine());
+DateTime dt = DateTime.Now;// текущее время
+long Sum = 0;
+//for (long i = 1; i <= N; i++)
+//{
+//    Sum = Sum + i;
+//}
+Sum = N * (N + 1) / 2;
+Console.WriteLine((DateTime.Now-dt).TotalMilliseconds);
+Console.WriteLine(Sum);
